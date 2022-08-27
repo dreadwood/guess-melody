6190148d2c4f82f986ce4a412a8cd32e86dfc1ac
srg <skabassjob@gmail.com>
2022-08-28 01:10:59 +0400

    3.1 add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import AuthScreen from '../auth-screen/auth-screen';
+import GameOverScreen from '../game-over-screen/game-over-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 import WelcomeScreenn from '../welcome-screen/welcome-screen';
+import WinScreen from '../win-screen/win-screen';
 
 type AppScreenProps = {
   errorsCount: number;
@@ -6,7 +14,50 @@ type AppScreenProps = {
 
 function App({errorsCount}: AppScreenProps): JSX.Element {
   return (
-    <WelcomeScreenn errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<WelcomeScreenn errorsCount={errorsCount} />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path={AppRoute.DevArtist}
+          element={<ArtistQuestionScreen />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path={AppRoute.DevGenre}
+          element={<GenreQuestionScreen />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path={AppRoute.Login}
+          element={<AuthScreen />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path={AppRoute.Lose}
+          element={<GameOverScreen />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path={AppRoute.Result}
+          element={<WinScreen />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path={AppRoute.NotFound}
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/const.ts
@@ -0,0 +1,9 @@
+export enum AppRoute {
+  Login = '/login',
+  Lose = '/lose',
+  Result = '/result',
+  Root = '/',
+  DevArtist = '/dev-artist',
+  DevGenre = '/dev-genre',
+  NotFound = '*',
+}
