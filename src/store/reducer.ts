9254e633852759e2cacaeb36f561f66f72f77be0
srg <skabassjob@gmail.com>
2022-09-02 17:53:36 +0400

    6.1 create initial state, actions and reducer from redux

---
+++ src/store/reducer.ts
@@ -0,0 +1,20 @@
+import {createReducer} from '@reduxjs/toolkit';
+import {FIRST_GAME_STEP} from '../const';
+import {incrementStep, resetGame} from './action';
+
+const STEP_COUNT = 1;
+
+const initialState = {
+  mistakes: 0,
+  step: FIRST_GAME_STEP,
+};
+
+export const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(incrementStep, (state) => {
+      state.step += STEP_COUNT;
+    })
+    .addCase(resetGame, (state) => {
+      state = {...initialState};
+    });
+});
