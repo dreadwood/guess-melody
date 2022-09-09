b0a934a52958351c105d716c805fd270ec0355a1
srg <skabassjob@gmail.com>
2022-09-09 23:18:10 +0400

    7.1 create token module

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,8 @@
 export const FIRST_GAME_STEP = 0;
 export const MAX_MISTAKE_COUNT = 3;
 
+export const AUTH_TOKEN_KEY_NAME = 'guess-melody-token';
+
 export enum AppRoute {
   Login = '/login',
   Lose = '/lose',
