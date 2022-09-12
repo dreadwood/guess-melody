562268117e407f32407c6e6ae5394d9a746b9964
srg <skabassjob@gmail.com>
2022-09-12 19:44:10 +0400

    7.11 update GameOverScreen and WinScreen

--- src/components/game-over-screen/game-over-screen.tsx
+++ src/components/game-over-screen/game-over-screen.tsx
@@ -1,4 +1,12 @@
+import {useNavigate} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import {useAppDispatch} from '../../hooks';
+import {resetGame} from '../../store/action';
+
 function GameOverScreen(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
   return (
     <section className="result">
       <div className="result__logo">
@@ -6,7 +14,14 @@ function GameOverScreen(): JSX.Element {
       </div>
       <h2 className="result__title">Какая жалость!</h2>
       <p className="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
-      <button className="replay" type="button">Попробовать ещё раз</button>
+      <button className="replay"
+        type="button"
+        onClick={() => {
+          dispatch(resetGame());
+          navigate(AppRoute.Game);
+        }}
+      >Попробовать ещё раз
+      </button>
     </section>
   );
 }
