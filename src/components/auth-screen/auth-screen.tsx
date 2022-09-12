99e6236ba87b4e2fe8a053074fe615e1beaecfa1
srg <skabassjob@gmail.com>
2022-09-12 19:41:46 +0400

    7.10 update AuthScreen

--- src/components/auth-screen/auth-screen.tsx
+++ src/components/auth-screen/auth-screen.tsx
@@ -1,4 +1,32 @@
+import {FormEvent, useRef} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import {useAppDispatch} from '../../hooks';
+import {loginAction} from '../../store/api-actions';
+import {AuthData} from '../../types/auth-data';
+
 function AuthScreen(): JSX.Element {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null ) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
   return (
     <section className="login">
       <div className="login__logo">
@@ -6,19 +34,31 @@ function AuthScreen(): JSX.Element {
       </div>
       <h2 className="login__title">Вы настоящий меломан!</h2>
       <p className="login__text">Хотите узнать свой результат? Представтесь!</p>
-      <form className="login__form" action="">
+      <form className="login__form"
+        onSubmit={handleSubmit}
+      >
         <p className="login__field">
           <label className="login__label" htmlFor="name">Логин</label>
-          <input className="login__input" type="text" name="name" id="name" />
+          <input className="login__input"
+            type="text" name="name" id="name"
+            ref={loginRef}
+          />
         </p>
         <p className="login__field">
           <label className="login__label" htmlFor="password">Пароль</label>
-          <input className="login__input" type="text" name="password" id="password" />
+          <input className="login__input"
+            type="text" name="password" id="password"
+            ref={passwordRef}
+          />
           <span className="login__error">Неверный пароль</span>
         </p>
         <button className="login__button button" type="submit">Войти</button>
       </form>
-      <button className="replay" type="button">Сыграть ещё раз</button>
+      <button className="replay"
+        type="button"
+        onClick={() => navigate(AppRoute.Game)}
+      >Сыграть ещё раз
+      </button>
     </section>
   );
 }
